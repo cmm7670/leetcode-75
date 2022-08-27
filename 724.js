702ce7dc0136cb1eda153ad41c5a975d9cad4f0a
cmm7670 <cmm7670@hotmail.com>
2022-08-26 22:43:05 -0500

    finishd 724

--- 724.js
+++ 724.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
  var pivotIndex = function(nums) {
-    
+    for( x = 0; x < nums.length; x++ ) {
+        const leftSum = nums.slice(0, x).reduce((prev, curr) => { return prev + curr }, 0);
+        const rightSum = nums.slice(x + 1).reduce((prev, curr) => { return prev + curr }, 0);
+        if(leftSum == rightSum) { return x };
+    }
+    return -1;
 };
+
+console.log(pivotIndex([1, 3, 5, 4, 2, -14]))
+console.log(pivotIndex([1, 3, 5, 4, 2, 7]))
