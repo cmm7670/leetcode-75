ee3b12b21005a255712746274b03dfc83b01ee9c
cmm7670 <cmm7670@hotmail.com>
2022-08-31 17:09:24 -0500

    it's worse

--- 205.js
+++ 205.js
@@ -16,14 +16,40 @@
         test[x] = [sCodes[x], tCodes[x]]
     }
 
-    for(x=0; x < test.length; x++) {
-        for(y=0; y < test.length; y++) {
-            if((JSON.stringify(test[x]) != JSON.stringify(test[y])) && (test[x][0] == test[y][0] || test[x][1] == test[y][1])) {
-                return false;
-            }
+    // for(x=0; x < test.length; x++) {
+    //     for(y=0; y < test.length; y++) {
+    //         if((JSON.stringify(test[x]) != JSON.stringify(test[y])) && (test[x][0] == test[y][0] || test[x][1] == test[y][1])) {
+    //             return false;
+    //         }
+    //     }
+    // }
+    // for(x = 0; x < test.length; x++) {
+    //     console.log(test);
+    //     let best = test.filter((ele, idx, arr) => {
+    //         return ele[0] == arr[x]
+    //     });
+    //     console.log(best);
+    // }
+
+    console.log(test);
+
+    let best = test.filter((ele, idx, arr) => {
+        for(x = 0; x < test.length; x++) {
+            return ele[0] == arr[x][0] && ele[1] == arr[x][1]
         }
-    }
+    });
+    console.log(best);
+
+    // for(x=0; x < test.length; x++) {
+    //     if(test.filter(value => value[x][0] ))
+    //     for(y=0; y < test.length; y++) {
+    //         if((JSON.stringify(test[x]) != JSON.stringify(test[y])) && (test[x][0] == test[y][0] || test[x][1] == test[y][1])) {
+    //             return false;
+    //         }
+    //     }
+    // }
     return true;
 };
 
 console.log(isIsomorphic('abab', 'baba'));
+console.log(isIsomorphic('geed', 'rwtt'));
